e7c94ba739be5d2978fa005d873ae22709a48759
Nata <rahanata9@gmail.com>
2019-10-17 09:59:49 +0700

    add

--- vendor/yiisoft/extensions.php
+++ vendor/yiisoft/extensions.php
@@ -93,4 +93,13 @@ return array (
       '@yii/gii' => $vendorDir . '/yiisoft/yii2-gii/src',
     ),
   ),
+  'yii2tech/spreadsheet' => 
+  array (
+    'name' => 'yii2tech/spreadsheet',
+    'version' => '1.0.5.0',
+    'alias' => 
+    array (
+      '@yii2tech/spreadsheet' => $vendorDir . '/yii2tech/spreadsheet/src',
+    ),
+  ),
 );
