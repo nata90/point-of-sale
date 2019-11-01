37ed23e61a3d7b773fe11b0cdca5ccc704554d9d
Nata <rahanata9@gmail.com>
2019-11-01 10:51:14 +0700

    add dashboard chart js

--- views/site/dashboard.php
+++ views/site/dashboard.php
@@ -2,165 +2,53 @@
 
 $this->registerJs(<<<JS
 	$(function () {
-		//-------------
-	    //- PIE CHART -
-	    //-------------
-	    // Get context with jQuery - using jQuery's .get() method.
-	    var pieChartCanvas = $('#pieChart').get(0).getContext('2d')
-	    var pieChart       = new Chart(pieChartCanvas)
-	    var PieData        = [
-	      {
-	        value    : 700,
-	        color    : '#f56954',
-	        highlight: '#f56954',
-	        label    : 'Chrome'
-	      },
-	      {
-	        value    : 500,
-	        color    : '#00a65a',
-	        highlight: '#00a65a',
-	        label    : 'IE'
-	      },
-	      {
-	        value    : 400,
-	        color    : '#f39c12',
-	        highlight: '#f39c12',
-	        label    : 'FireFox'
-	      },
-	      {
-	        value    : 600,
-	        color    : '#00c0ef',
-	        highlight: '#00c0ef',
-	        label    : 'Safari'
-	      },
-	      {
-	        value    : 300,
-	        color    : '#3c8dbc',
-	        highlight: '#3c8dbc',
-	        label    : 'Opera'
-	      },
-	      {
-	        value    : 100,
-	        color    : '#d2d6de',
-	        highlight: '#d2d6de',
-	        label    : 'Navigator'
-	      }
-	    ]
-	    var pieOptions     = {
-	      //Boolean - Whether we should show a stroke on each segment
-	      segmentShowStroke    : true,
-	      //String - The colour of each segment stroke
-	      segmentStrokeColor   : '#fff',
-	      //Number - The width of each segment stroke
-	      segmentStrokeWidth   : 2,
-	      //Number - The percentage of the chart that we cut out of the middle
-	      percentageInnerCutout: 50, // This is 0 for Pie charts
-	      //Number - Amount of animation steps
-	      animationSteps       : 100,
-	      //String - Animation easing effect
-	      animationEasing      : 'easeOutBounce',
-	      //Boolean - Whether we animate the rotation of the Doughnut
-	      animateRotate        : true,
-	      //Boolean - Whether we animate scaling the Doughnut from the centre
-	      animateScale         : false,
-	      //Boolean - whether to make the chart responsive to window resizing
-	      responsive           : true,
-	      // Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
-	      maintainAspectRatio  : true,
-	      //String - A legend template
-	      legendTemplate       : '<ul class="<%=name.toLowerCase()%>-legend"><% for (var i=0; i<segments.length; i++){%><li><span style="background-color:<%=segments[i].fillColor%>"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>'
-	    }
-	    //Create pie or douhnut chart
-	    // You can switch between pie and douhnut using the method below.
-	    pieChart.Doughnut(PieData, pieOptions)
-
-		var areaChartData = {
-	      labels  : ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-	      datasets: [
-	        {
-	          label               : 'Electronics',
-	          fillColor           : 'rgba(210, 214, 222, 1)',
-	          strokeColor         : 'rgba(210, 214, 222, 1)',
-	          pointColor          : 'rgba(210, 214, 222, 1)',
-	          pointStrokeColor    : '#c1c7d1',
-	          pointHighlightFill  : '#fff',
-	          pointHighlightStroke: 'rgba(220,220,220,1)',
-	          data                : [65, 59, 80, 81, 56, 55, 40]
-	        },
-	        {
-	          label               : 'Digital Goods',
-	          fillColor           : 'rgba(60,141,188,0.9)',
-	          strokeColor         : 'rgba(60,141,188,0.8)',
-	          pointColor          : '#3b8bba',
-	          pointStrokeColor    : 'rgba(60,141,188,1)',
-	          pointHighlightFill  : '#fff',
-	          pointHighlightStroke: 'rgba(60,141,188,1)',
-	          data                : [28, 48, 40, 19, 86, 27, 90]
-	        }
-	      ]
-	    }
-
-		//-------------
-    	//- BAR CHART -
-    	//-------------
-    	var barChartCanvas                   = $('#barChart').get(0).getContext('2d')
-    	var barChart                         = new Chart(barChartCanvas)
-	    var barChartData                     = areaChartData
-	    barChartData.datasets[1].fillColor   = '#00a65a'
-	    barChartData.datasets[1].strokeColor = '#00a65a'
-	    barChartData.datasets[1].pointColor  = '#00a65a'
-	    var barChartOptions                  = {
-	      //Boolean - Whether the scale should start at zero, or an order of magnitude down from the lowest value
-	      scaleBeginAtZero        : true,
-
-	      //Boolean - Whether grid lines are shown across the chart
-	      scaleShowGridLines      : true,
-
-	      //String - Colour of the grid lines
-	      scaleGridLineColor      : 'rgba(0,0,0,.05)',
-
-	      //Number - Width of the grid lines
-	      scaleGridLineWidth      : 1,
-
-	      //Boolean - Whether to show horizontal lines (except X axis)
-	      scaleShowHorizontalLines: true,
-
-	      //Boolean - Whether to show vertical lines (except Y axis)
-	      scaleShowVerticalLines  : true,
-
-	      //Boolean - If there is a stroke on each bar
-	      barShowStroke           : true,
-
-	      //Number - Pixel width of the bar stroke
-	      barStrokeWidth          : 2,
-
-	      //Number - Spacing between each of the X value sets
-	      barValueSpacing         : 5,
-
-	      //Number - Spacing between data sets within X values
-	      barDatasetSpacing       : 1,
-
-	      //String - A legend template
-	      legendTemplate          : '<ul class="<%=name.toLowerCase()%>-legend"><% for (var i=0; i<datasets.length; i++){%><li><span style="background-color:<%=datasets[i].fillColor%>"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>',
-
-	      //Boolean - whether to make the chart responsive
-	      responsive              : true,
-	      maintainAspectRatio     : true
-	    }
-
-	    barChartOptions.datasetFill = false
-	    barChart.Bar(barChartData, barChartOptions)
+		var ctx = document.getElementById('barChart').getContext('2d');
+		var myChart = new Chart(ctx, {
+		    type: 'bar',
+		    data: {
+		        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+		        datasets: [{
+		            label: '# of Votes',
+		            data: [12, 19, 3, 5, 2, 3],
+		            backgroundColor: [
+		                'rgba(255, 99, 132, 0.2)',
+		                'rgba(54, 162, 235, 0.2)',
+		                'rgba(255, 206, 86, 0.2)',
+		                'rgba(75, 192, 192, 0.2)',
+		                'rgba(153, 102, 255, 0.2)',
+		                'rgba(255, 159, 64, 0.2)'
+		            ],
+		            borderColor: [
+		                'rgba(255, 99, 132, 1)',
+		                'rgba(54, 162, 235, 1)',
+		                'rgba(255, 206, 86, 1)',
+		                'rgba(75, 192, 192, 1)',
+		                'rgba(153, 102, 255, 1)',
+		                'rgba(255, 159, 64, 1)'
+		            ],
+		            borderWidth: 1
+		        }]
+		    },
+		    options: {
+		        scales: {
+		            yAxes: [{
+		                ticks: {
+		                    beginAtZero: true
+		                }
+		            }]
+		        }
+		    }
+		});
 	})
 JS
 );
 ?>
 	<div class="row">
-	    <div class="col-md-6">
-	       
-          <!-- BAR CHART -->
+		 <!-- BAR CHART -->
+	    <div class="col-md-8">
           <div class="box box-danger">
             <div class="box-header with-border">
-              <h3 class="box-title">Bar Chart</h3>
+              <h3 class="box-title">Penjualan</h3>
 
               <div class="box-tools pull-right">
                 <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
@@ -173,30 +61,30 @@ JS
                 <canvas id="barChart" style="height:230px"></canvas>
               </div>
             </div>
-            <!-- /.box-body -->
           </div>
-          <!-- /.box -->
-
-          <!-- /.box -->
 	    </div>
-	    <div class="col-md-6">
-          	<!-- DONUT CHART -->
-	          <div class="box box-danger">
-	            <div class="box-header with-border">
-	              <h3 class="box-title">Donut Chart</h3>
-
-	              <div class="box-tools pull-right">
-	                <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-	                </button>
-	                <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
-	              </div>
-	            </div>
-	            <div class="box-body">
-	              <canvas id="pieChart" style="height:250px"></canvas>
-	            </div>
-	            <!-- /.box-body -->
-	          </div>
-          </div>
+	    <div class="col-md-4">
+	    	<div class="box box-danger">
+	    		<div class="box-header with-border">
+		    		<h3 class="box-title">10 TERLARIS BULAN INI</h3>
+		    	</div>
+	    		<div class="box-body">
+	    			<ul class="todo-list ui-sortable">
+	    			    <li><span class="handle ui-sortable-handle"><i class="fa fa-ellipsis-v"></i><i class="fa fa-ellipsis-v"></i></span><span class="text">TV</span><span class="label label-success">20 ITEM</span></li>
+	    			    <li><span class="handle ui-sortable-handle"><i class="fa fa-ellipsis-v"></i><i class="fa fa-ellipsis-v"></i></span><span class="text">KULKAS 2 PINTU</span><span class="label label-warning">20 ITEM</span></li>
+	    			    <li><span class="handle ui-sortable-handle"><i class="fa fa-ellipsis-v"></i><i class="fa fa-ellipsis-v"></i></span><span class="text">KOMPUTER</span><span class="label label-danger">15 ITEM</span></li>
+	    			   <li><span class="handle ui-sortable-handle"><i class="fa fa-ellipsis-v"></i><i class="fa fa-ellipsis-v"></i></span><span class="text">SEPEDA MOTOR</span><span class="label label-primary">5 ITEM</span></li>
+	    			    <li><span class="handle ui-sortable-handle"><i class="fa fa-ellipsis-v"></i><i class="fa fa-ellipsis-v"></i></span><span class="text">SEPEDA MOTOR</span><span class="label label-primary">5 ITEM</span></li>
+	    			    <li><span class="handle ui-sortable-handle"><i class="fa fa-ellipsis-v"></i><i class="fa fa-ellipsis-v"></i></span><span class="text">TV</span><span class="label label-success">20 ITEM</span></li>
+	    			    <li><span class="handle ui-sortable-handle"><i class="fa fa-ellipsis-v"></i><i class="fa fa-ellipsis-v"></i></span><span class="text">KULKAS 2 PINTU</span><span class="label label-warning">20 ITEM</span></li>
+	    			    <li><span class="handle ui-sortable-handle"><i class="fa fa-ellipsis-v"></i><i class="fa fa-ellipsis-v"></i></span><span class="text">KOMPUTER</span><span class="label label-danger">15 ITEM</span></li>
+	    			    <li><span class="handle ui-sortable-handle"><i class="fa fa-ellipsis-v"></i><i class="fa fa-ellipsis-v"></i></span><span class="text">TV</span><span class="label label-success">20 ITEM</span></li>
+	    			    <li><span class="handle ui-sortable-handle"><i class="fa fa-ellipsis-v"></i><i class="fa fa-ellipsis-v"></i></span><span class="text">KULKAS 2 PINTU</span><span class="label label-warning">20 ITEM</span></li>
+	    			</ul>
+	    		</div>
+	    	</div>
+	    </div>
+	    
 	</div>
 
 
